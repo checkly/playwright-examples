c290e7da785892e237cb58367ef59f4fa5ac4d6a
stefan judis <stefanjudis@gmail.com>
2024-07-24 00:25:39 +0200

    Add "type checking in PWT projects"

--- pwt-playwright-type-check-and-lint/tests/example.spec.ts
+++ pwt-playwright-type-check-and-lint/tests/example.spec.ts
@@ -1,18 +1,16 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from "@playwright/test"
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("test with a type error", async ({ page }) => {
+  await page.goto("https://playwright.dev/")
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisibles()
+})
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.skip("test with incorrect promise handling", async ({ page }) => {
+  await page.goto("https://playwright.dev/")
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  const button = await page.getByRole("link", { name: "Get started" })
+  button.click()
+})
